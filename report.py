55b803369b26ca10d0ebebe2b2e049d7520a4291
unionppp-tech <unionppp@gmail.com>
2026-02-28 15:19:40 +0900

    Update report.py

--- report.py
+++ report.py
@@ -42,6 +42,7 @@ def analyze_ticker(ticker):
         f"60일 평균: {ma_60:.2f}$ | "
         f"표준편차 타겟가: {target_std_60:.2f}$ | "
         f"표준편차 비율(1미만 매수): {rate_target_std_60:.4f}"
+        prift("=================================")
     )
 
 def main():
